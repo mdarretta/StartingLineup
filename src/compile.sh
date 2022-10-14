463600e5656b0e2b9ef9d01661144eac7765b8e5
mdarretta <52295663+mdarretta@users.noreply.github.com>
2022-10-14 07:59:30 -0500

    Delete compile.sh

--- src/compile.sh
+++
@@ -1,2 +0,0 @@
-javac -Xlint:unchecked -d ../classes org/startinglineup/component/*.java org/startinglineup/event/*.java org/startinglineup/Driver.java org/startinglineup/simulator/*.java org/startinglineup/controller/*.java
-
