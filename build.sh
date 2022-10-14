dffbbb1399f9fd5ac257cbb8d33ef4b275ab310c
mdarretta <52295663+mdarretta@users.noreply.github.com>
2022-10-14 09:09:48 -0500

    Update build.sh
    Accidentally removed "src" directory. Change to "org".

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 SRC_PATH=./src/org/startinglineup
 
 #Clean generated directories
-rm -rf doc src
+rm -rf doc org
 
 javac -Xlint:unchecked -d .  $SRC_PATH/*.java $SRC_PATH/component/*.java $SRC_PATH/controller/*.java $SRC_PATH/data/*.java $SRC_PATH/event/*.java $SRC_PATH/league/*.java $SRC_PATH/simulator/*.java $SRC_PATH/utils/*.java 
 
