bd5c7aed39e6093b1319522aa8dd71325552fe7b
mdarretta <52295663+mdarretta@users.noreply.github.com>
2022-10-14 09:07:53 -0500

    Update build.sh
    1. Clean generated directories.
    2. (Temp fix) Driver.java javadoc not being generated. Reference directly to generate.

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 SRC_PATH=./src/org/startinglineup
 
+#Clean generated directories
+rm -rf doc src
+
 javac -Xlint:unchecked -d .  $SRC_PATH/*.java $SRC_PATH/component/*.java $SRC_PATH/controller/*.java $SRC_PATH/data/*.java $SRC_PATH/event/*.java $SRC_PATH/league/*.java $SRC_PATH/simulator/*.java $SRC_PATH/utils/*.java 
 
 jar cfe startinglineup.jar org.startinglineup.Driver ./org/startinglineup/* ./conf/* ./data/* ./doc/*
 
-javadoc -sourcepath $SRC_PATH/*.java $SRC_PATH/component/*.java $SRC_PATH/controller/*.java $SRC_PATH/data/*.java $SRC_PATH/event/*.java $SRC_PATH/league/*java $SRC_PATH/simulator/*.java $SRC_PATH/utils/*.java -d doc
+javadoc -sourcepath $SRC_PATH/Driver.java $SRC_PATH/*.java $SRC_PATH/component/*.java $SRC_PATH/controller/*.java $SRC_PATH/data/*.java $SRC_PATH/event/*.java $SRC_PATH/league/*java $SRC_PATH/simulator/*.java $SRC_PATH/utils/*.java -d doc
