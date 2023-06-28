e15645c21a3e613d568c24be1f9cfcf9d3adc104
Mike Darretta <52295663+mdarretta@users.noreply.github.com>
2023-06-28 08:27:47 -0500

    Remove extraneous comment

--- src/main/java/org/startinglineup/StartingLineup.java
+++ src/main/java/org/startinglineup/StartingLineup.java
@@ -5,7 +5,6 @@ import org.startinglineup.gui.MainWindow;
 public class StartingLineup {
 	
     public static void main(String[] args) {
-        //new MainWindow().show();
         new MainWindow().setVisible(true);
     }
 }
